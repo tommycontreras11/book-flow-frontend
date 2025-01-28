f4753899f071d474ba10f9a6f9b097c94dd8d173
TommyGC11 <tommycontreras34@gmail.com>
2025-01-28 15:14:29 -0400

    feat: :sparkles: add request enum file and add person type to common enum file

--- src/enums/common.enum.ts
+++ src/enums/common.enum.ts
@@ -2,3 +2,8 @@ export enum StatusEnum {
   ACTIVE = "ACTIVE",
   INACTIVE = "INACTIVE",
 }
+
+export enum PersonTypeEnum {
+  NATURAL = 'NATURAL',
+  LEGAL = 'LEGAL'
+}
---
+++ src/enums/request.enum.ts
@@ -0,0 +1,5 @@
+export enum StatusRequestEnum {
+  APPROVAL = "APPROVAL",
+  DENIED = "DENIED",
+  PENDING = "PENDING",
+}
