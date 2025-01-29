c867ee688733abfb332c217d3cf349007c6588d9
TommyGC11 <tommycontreras34@gmail.com>
2025-01-29 16:40:16 -0400

    feat: :sparkles: add services to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,14 @@ export default function Home() {
   return (
     <div className="mx-auto w-full max-w-2xl overflow-x-auto">
       <a href="/language">Go to language page</a>
+      <br />
+      <a href="/bibliography-type">Go to bibliography type page</a>
+      <br />
+      <a href="/science">Go to science page</a>
+      <br />
+      <a href="/publisher">Go to publisher page</a>
+      <br />
+      <a href="/author">Go to author page</a>
     </div>
   );
 }
