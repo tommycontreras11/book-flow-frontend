d3312b835edf09e5cd23305eace7a633ef9d0dcc
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-28 20:30:06 -0400

    feat: :sparkles: add create or update components

---
+++ src/app/language/page.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { ICreateLanguage, ILanguage, IUpdateLanguage } from "@/interfaces/language.interface";
+import { deleteLanguage, getAllLanguage } from "@/lib/language.lib";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import { IMessage } from "@/interfaces/message.interface";
+import { CreateUpdateForm, IFormField } from "@/components/common/create";
+
+export default function Language() {
+  let [languages, setLanguages] = useState<ILanguage[]>([]);
+  
+  const languageFields: IFormField[] = [
+    { name: "description", label: "Description", type: "text" },
+  ];
+
+  const fetchLanguages = async () => {
+    getAllLanguage()
+      .then((languages) => {
+        setLanguages(languages.data);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    fetchLanguages();
+  }, []);
+
+  const handleDelete = (uuid: string) => {
+    deleteLanguage(uuid).then((data: IMessage) => {
+      fetchLanguages();
+      console.log(data.message);
+    }).catch((err) => console.log(err));
+  }
+
+  const handleUpdate = (uuid: string) => {
+    console.log(uuid);
+  }
+
+  const handleSave = async (formData: ICreateLanguage | IUpdateLanguage) => {
+    console.log(formData);
+    fetchLanguages();
+  };
+
+  return (
+    <div className="mx-auto w-full max-w-2xl overflow-x-auto">
+      <CreateUpdateForm<ICreateLanguage>
+        isEditable={false}
+        entityName="Language"
+        fields={languageFields}
+        onSubmit={handleSave}
+      />
+      <Table>
+        <TableCaption>Languages List</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>UUID</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {languages.map((language) => (
+            <TableRow key={language.uuid}>
+              <TableCell>{language.uuid}</TableCell>
+              <TableCell>{language.description}</TableCell>
+              <TableCell>{language.status}</TableCell>
+              
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                      <span className="sr-only">Open menu</span>
+                      <MoreHorizontal />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="start">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem
+                      onClick={() => handleUpdate(language.uuid)}
+                    >
+                      Update
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      onClick={() => handleDelete(language.uuid)}
+                    >
+                      Delete
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
