d01d57e9978a03283c1b9287572b8db76670cca7
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-02-01 19:46:16 -0400

    feat: :sparkles: implement middleware to validate protect routes

---
+++ src/middleware.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { me } from "./lib/auth.lib";
+
+const protectedRoutes = [
+  "/language",
+  "/bibliography-type",
+  "/science",
+  "/publisher",
+  "/author",
+  "country",
+  "/book"
+];
+
+// Helper function to check if a path is protected
+function isProtectedRoute(path: string): boolean {
+  return protectedRoutes.some((route) => path.startsWith(route));
+}
+
+export async function middleware(request: NextRequest) {
+    const user = await me();
+
+    const currentPath = request.nextUrl.pathname;
+
+    if (currentPath === "/auth/signIn") {
+        return NextResponse.next();
+    }
+    
+      if (isProtectedRoute(currentPath) && user?.message) {
+        console.log('request.url: ', request.url);
+        return NextResponse.redirect(new URL("auth/signIn", request.url));
+      }
+    
+      return NextResponse.next();
+}
+
+// Optionally, you can add a matcher to optimize performance
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
