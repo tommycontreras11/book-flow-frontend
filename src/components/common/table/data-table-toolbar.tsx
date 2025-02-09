2afce5c56e4c2138780ca213ce0c2348b38263cd
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-02-09 11:36:07 -0400

    feat: :sparkles: add function to prevent column undefined

--- src/components/common/table/data-table-toolbar.tsx
+++ src/components/common/table/data-table-toolbar.tsx
@@ -15,32 +15,45 @@ interface DataTableToolbarProps<TData> {
   definitions: IStatusTableDefinitions[];
 }
 
-export function DataTableToolbar<TData>({ table, definitions }: DataTableToolbarProps<TData>) {
+const verifyIfColumnExists = (table: Table<any>, column: string): boolean => {
+  return !!table.getAllColumns().find((col) => col.id === column);
+};
+
+export function DataTableToolbar<TData>({
+  table,
+  definitions,
+}: DataTableToolbarProps<TData>) {
   const isFiltered = table.getState().columnFilters.length > 0;
+
+  const nameColumn = verifyIfColumnExists(table, "name") ? table?.getColumn("name") : null;
 
   return (
     <div className="flex w-full items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
-        <Input
-          placeholder={"Filter"}
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-          onChange={(event) => table.getColumn("name")?.setFilterValue(event.target.value)}
-          className="h-8 w-[150px] lg:w-[250px]"
-        />
+        {nameColumn && (
+          <Input
+            placeholder={"Filter"}
+            value={(nameColumn?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              nameColumn?.setFilterValue(event.target.value)
+            }
+            className="h-8 w-[150px] lg:w-[250px]"
+          />
+        )}
 
-        {table.getColumn("status") && (
           <DataTableFacetedFilter
-            column={table.getColumn("status")}
+            column={table?.getColumn("status")}
             title={"Status"}
             options={definitions}
           />
-        )}
+        
 
         {isFiltered && (
           <Button
             variant="outline"
             onClick={() => table.resetColumnFilters()}
-            className="h-8 px-2 lg:px-3">
+            className="h-8 px-2 lg:px-3"
+          >
             {"Clean Filters"}
             <Cross2Icon className="ml-2 h-4 w-4" />
           </Button>
