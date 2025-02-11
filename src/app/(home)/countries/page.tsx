ecd4c63ded9be0e59af7f0a9eac6a98235a2b1d2
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-02-10 20:48:36 -0400

    feat: :sparkles: add country custom table with filters

--- src/app/(home)/countries/page.tsx
+++ src/app/(home)/countries/page.tsx
@@ -1,24 +1,46 @@
 "use client";
 
 import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+  CreateUpdateForm,
+  IFormField,
+} from "@/components/common/modal/create-update";
+import DataTable from "@/components/common/table/data-table";
+import { commonStatusTableDefinitions } from "@/definitions/common.definition";
 import {
-  ICountry
+  ICountry,
+  ICreateCountry,
+  IUpdateCountry,
 } from "@/interfaces/country.interface";
+import { IMessage } from "@/interfaces/message.interface";
 import {
-  getAllCountries
+  createCountry,
+  deleteCountry,
+  getAllCountries,
+  getOneCountry,
+  updateCountry,
 } from "@/lib/country.lib";
+import { fillFormInput } from "@/lib/utils";
+import { countryFormSchema } from "@/schema/country.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { columns } from "./table/column";
 
 export default function Country() {
   const [countries, setCountries] = useState<ICountry[]>([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditable, setIsEditable] = useState(false);
+  const [uuid, setUUID] = useState("");
+  const [countryFields, setCountryFields] = useState<IFormField[]>([
+    { name: "name", label: "Name", type: "text" },
+  ]);
+
+  const form = useForm<ICreateCountry | IUpdateCountry>({
+    resolver: zodResolver(countryFormSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
 
   const fetchCountries = async () => {
     getAllCountries()
@@ -27,30 +49,86 @@ export default function Country() {
       })
       .catch((err) => console.log(err));
   };
-
+  
   useEffect(() => {
     fetchCountries();
-  }, []);
+  }, [])
+
+  const handleDelete = (uuid: string) => {
+    deleteCountry(uuid)
+      .then((data: IMessage) => {
+        fetchCountries();
+        console.log(data.message);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleUpdate = (uuid: string) => {
+    getOneCountry(uuid)
+      .then((country) => {
+        fillFormInput(form, [{ property: "name", value: country.data.name }]);
+        setIsEditable(true);
+        setIsModalOpen(true);
+        setUUID(uuid);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const modifyCountry = (country: IUpdateCountry) => {
+    updateCountry(uuid, country)
+      .then((data: IMessage) => {
+        form.reset();
+        setIsEditable(false);
+        setIsModalOpen(false);
+        console.log(data.message);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const saveCountry = (country: ICreateCountry) => {
+    createCountry(country)
+      .then((data: IMessage) => {
+        form.reset();
+        setIsModalOpen(false);
+        console.log(data.message);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleSubmit = async (formData: ICreateCountry | IUpdateCountry) => {
+    if (uuid) {
+      modifyCountry(formData);
+      fetchCountries();
+      return;
+    }
+
+    saveCountry(formData as ICreateCountry);
+    fetchCountries();
+  };
+
   return (
     <div className="mx-auto w-full max-w-2xl overflow-x-auto">
-      <Table>
-        <TableCaption>Countries List</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>UUID</TableHead>
-            <TableHead>Description</TableHead>
-            <TableHead>Status</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {countries.map((country) => (
-            <TableRow key={country.uuid}>
-              <TableCell>{country.uuid}</TableCell>
-              <TableCell>{country.name}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <button
+        className="bg-sky-700 hover:bg-sky-800 text-white font-bold py-2 px-4 rounded mb-4"
+        onClick={() => setIsModalOpen(true)}
+      >
+        Create
+      </button>
+      <DataTable
+        data={countries}
+        columns={columns({ handleUpdate, handleDelete })}
+        definitions={commonStatusTableDefinitions}
+      />
+
+      {isModalOpen && (
+        <CreateUpdateForm<ICreateCountry | IUpdateCountry>
+          isEditable={isEditable}
+          entityName="Country"
+          fields={countryFields}
+          form={form}
+          onSubmit={handleSubmit}
+        />
+      )}
     </div>
   );
 }
--- src/lib/country.lib.ts
+++ src/lib/country.lib.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { ICreateCountry, IUpdateCountry } from "@/interfaces/country.interface";
 import api from "./api";
 
 import { config } from "./config";
@@ -16,6 +17,36 @@ export const getAllCountries = async () => {
 export const getOneCountry = async (uuid: string) => {
   try {
     const response = await api.get(config.apiURL + "/countries/" + uuid);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createCountry = async (author: ICreateCountry) => {
+  try {
+    const response = await api.post(config.apiURL + "/countries", author);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateCountry = async (uuid: string, author: IUpdateCountry) => {
+  try {
+    const response = await api.patch(
+      config.apiURL + "/countries/" + uuid,
+      author
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteCountry = async (uuid: string) => {
+  try {
+    const response = await api.delete(config.apiURL + "/countries/" + uuid);
     return response.data;
   } catch (error) {
     console.log(error);
