89e2e33041b0bd43dc253be8b1fe3564cbe270bb
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-03-28 11:20:58 -0400

    feat: :sparkles: update clearForm utility to conditionally close modal based on parameter

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -1,9 +1,9 @@
 import { Dispatch, SetStateAction } from 'react';
 import { FieldValues, UseFormReturn } from 'react-hook-form';
 
-export const clearForm = <T extends FieldValues>(form: UseFormReturn<T>, setIsModalOpen: Dispatch<SetStateAction<boolean>>, setIsEditable: Dispatch<SetStateAction<boolean>>, setUUID: Dispatch<SetStateAction<string | null>>) => {
+export const clearForm = <T extends FieldValues>(form: UseFormReturn<T>, closeModal = false, setIsModalOpen: Dispatch<SetStateAction<boolean>>, setIsEditable: Dispatch<SetStateAction<boolean>>, setUUID: Dispatch<SetStateAction<string | null>>) => {
     form.reset();
-    setIsModalOpen(false);
+    closeModal && setIsModalOpen(false);
     setIsEditable(false);
     setUUID(null);
   }
