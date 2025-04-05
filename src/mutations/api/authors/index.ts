a944315112f8d508125619c374bc4f193aef5fa2
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-04-05 11:35:28 -0400

    feat: :sparkles: add onSuccess callback support to useCreateAuthor, useUpdateAuthor, and useDeleteAuthor functions

--- src/mutations/api/authors/index.ts
+++ src/mutations/api/authors/index.ts
@@ -3,28 +3,34 @@ import { getMutationOptions } from "@/utils/reactQueryOptions";
 import authorsProvider from "@/providers/http/authors";
 import { ICreateAuthor, IUpdateAuthor } from "@/providers/http/authors/interface";
 
-export function useCreateAuthor() {
+export function useCreateAuthor(onSuccessCallback?: (data: any) => void) {
   const queryClient = useQueryClient();
   return useMutation(
     (data: ICreateAuthor) => authorsProvider.create(data),
-    getMutationOptions(queryClient, "authors")
+    getMutationOptions(queryClient, "authors", null, {
+      onSuccess: onSuccessCallback,
+    })
   );
 }
 
-export function useUpdateAuthor() {
+export function useUpdateAuthor(onSuccessCallback?: (data: any) => void) {
   const queryClient = useQueryClient();
   return useMutation(
     ({ uuid, data }: { uuid: string; data: IUpdateAuthor }) =>
       authorsProvider.update(uuid, data),
-    getMutationOptions(queryClient, "authors")
+    getMutationOptions(queryClient, "authors", "author", {
+      onSuccess: onSuccessCallback,
+    })
   );
 }
 
-export function useDeleteAuthor() {
+export function useDeleteAuthor(onSuccessCallback?: (data: any) => void) {
   const queryClient = useQueryClient();
   return useMutation(
     (uuid: string) =>
       authorsProvider.destroy(uuid),
-    getMutationOptions(queryClient, "authors")
+    getMutationOptions(queryClient, "authors", "author", {
+      onSuccess: onSuccessCallback,
+    })
   );
 }
