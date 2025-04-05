0ab10615ac971525edf488a11f46931307337e09
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-04-05 13:56:09 -0400

    fix: :bug: update date selection logic to handle undefined values correctly

--- src/components/common/modal/create-update.tsx
+++ src/components/common/modal/create-update.tsx
@@ -142,7 +142,7 @@ export function CreateUpdateForm<T extends FieldValues>({
                           <PopoverContent className="w-auto p-0" align="start">
                             <Calendar
                               mode="single"
-                              selected={field.value ?? new Date()}
+                              selected={field.value ?? undefined}
                               onSelect={field.onChange}
                               disabled={(date) => {
                                 if (fieldInput.blockDatesAfterToday) { 
