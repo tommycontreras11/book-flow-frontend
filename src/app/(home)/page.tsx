920ad72aefc88fad6c727ef6269175a9a14264a7
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-04-08 14:02:57 -0400

    feat: :sparkles: implement book filtering functionality with modal and dynamic options

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,31 +4,65 @@ import BookCard from "@/components/common/card/book";
 import { QuickStatsCard } from "@/components/common/card/quick-stats";
 import { RecentActivitiesCard } from "@/components/common/card/recent-activities";
 import { TopBorrowedBooksCard } from "@/components/common/card/top-borrowed-books";
+import {
+  CreateUpdateForm,
+  IFormField,
+} from "@/components/common/modal/create-update";
+import { Button } from "@/components/ui/button";
 import { useAuth } from "@/contexts/auth-context";
 import { useGetAllBook, useGetAllBookStat } from "@/hooks/api/book.hook";
+import { useGetAllScience } from "@/hooks/api/science.hook";
 import { toast } from "@/hooks/use-toast";
 import { IMessage } from "@/interfaces/message.interface";
 import { ICreateRequest } from "@/interfaces/request.interface";
 import { createRequest } from "@/lib/request.lib";
-import { useMemo } from "react";
+import { IBookFilter } from "@/providers/http/books/interface";
+import { bookFilterSchema } from "@/schema/book.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SlidersHorizontal } from "lucide-react";
+import { useEffect, useMemo, useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { user } = useAuth();
+  const [science, setScience] = useState<string | null>(null);
   const isEmployee = useMemo(() => user?.role === "EMPLOYEE", [user]);
+
+  const [filterFields, setFilterFields] = useState<IFormField[]>([]);
+
+  const form = useForm<IBookFilter>({
+    resolver: zodResolver(bookFilterSchema),
+    defaultValues: {
+      science: "",
+    },
+  });
 
   const {
     data: books,
     error: bookError,
     isLoading: isLoadingBook,
     refetch,
-  } = useGetAllBook();
+  } = useGetAllBook(science);
+
+  const isAnyBookAvailable = useMemo(
+    () =>
+      !!books?.filter(
+        (book) =>
+          !book.requests.length ||
+          book?.requests.every((request) => request?.user?.uuid !== user?.uuid)
+      )?.length,
+    [books]
+  );
+
+  const { data: sciences, isLoading: isLoadingSciences } = useGetAllScience();
 
   const {
     data: booksStats,
     error: bookStatError,
     isLoading: isLoadingBookStat,
   } = useGetAllBookStat(user?.role === "EMPLOYEE");
-  
+
   const saveRequest = (request: ICreateRequest) => {
     createRequest(request)
       .then((data: IMessage) => {
@@ -63,6 +97,42 @@ export default function Home() {
     refetch();
   };
 
+  const handleSubmit = ({ science }: IBookFilter) => {
+    if (!science) return;
+
+    setScience(science);
+    form.reset();
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    if (isLoadingSciences) return;
+
+    setFilterFields((prevFields) => {
+      if (!prevFields.find((field) => field.name === "science")) {
+        return [
+          ...prevFields,
+          {
+            name: "science",
+            label: "Science",
+            type: "select",
+            options: [
+              {
+                label: "All",
+                value: "All",
+              },
+              ...(sciences?.map((science) => ({
+                label: science.name,
+                value: science.name,
+              })) || []),
+            ],
+          },
+        ];
+      }
+      return prevFields;
+    });
+  }, [sciences]);
+
   return (
     <>
       {!isLoadingBookStat && isEmployee && booksStats && (
@@ -76,26 +146,67 @@ export default function Home() {
           </>
         </div>
       )}
-      <div className="flex-1 rounded-xl md:min-h-min flex justify-center md:justify-start">
-        <div className="w-full max-w-6xl flex flex-wrap gap-4 items-center md:items-start justify-center md:justify-start">
-          {!isLoadingBook &&
-            books
-              ?.filter(
-                (book) =>
-                  !book.requests.length ||
-                  book?.requests.every(
-                    (request) => request?.user?.uuid !== user?.uuid
+
+      <div className={`flex flex-col ${isAnyBookAvailable ? "items-end" : "items-center"} w-full max-w-6xl mx-auto px-4`}>
+        {/* Filter Button */}
+        {/* <div className="mb-3">
+          <Button
+            className="hover:text-sky-800 text-sky-700 font-bold py-2 px-4 rounded"
+            variant={"outline"}
+            onClick={() => setIsModalOpen(true)}
+          >
+            <SlidersHorizontal className="mr-2" />
+            Filter
+          </Button>
+        </div> */}
+
+        {isAnyBookAvailable ? (
+          <>
+            <div className="mb-3">
+              <Button
+                className="hover:bg-sky-800 bg-sky-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => setIsModalOpen(true)}
+              >
+                <SlidersHorizontal className="mr-2" />
+                Filter
+              </Button>
+            </div>
+            <CreateUpdateForm<IBookFilter>
+              isEditable={false}
+              entityName="Filters"
+              fields={filterFields}
+              form={form}
+              onSubmit={handleSubmit}
+              isOpen={isModalOpen}
+              onClose={() => setIsModalOpen(false)}
+            />
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 w-full">
+              {!isLoadingBook &&
+                books
+                  ?.filter(
+                    (book) =>
+                      !book.requests.length ||
+                      book?.requests.every(
+                        (request) => request?.user?.uuid !== user?.uuid
+                      )
                   )
-              )
-              .map((book) => (
-                <BookCard
-                  key={book.uuid}
-                  book={book}
-                  user={user || undefined}
-                  handleSubmit={() => handleRequestBook(book.uuid)}
-                />
-              ))}
-        </div>
+                  .map((book) => (
+                    <div key={book.uuid} className="w-full">
+                      <BookCard
+                        book={book}
+                        user={user || undefined}
+                        handleSubmit={() => handleRequestBook(book.uuid)}
+                      />
+                    </div>
+                  ))}
+            </div>
+          </>
+        ) : (
+          <h2 className="text-2xl text-center font-medium mb-6">
+            No books available
+          </h2>
+        )}
       </div>
     </>
   );
