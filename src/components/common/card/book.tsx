121e5214da6b42f37a956492f5961a332ca35870
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-04-08 14:02:31 -0400

    feat: :sparkles: enhance BookCard component with request handling and user role support

--- src/components/common/card/book.tsx
+++ src/components/common/card/book.tsx
@@ -1,19 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { UserRoleEnum } from "@/enums/common.enum";
+import { StatusRequestEnum } from "@/enums/request.enum";
 import { IMeUser } from "@/interfaces/auth.interface";
 import { IBook } from "@/providers/http/books/interface";
+import { IRequest } from "@/providers/http/requests/interface";
 
 export default function BookCard({
   book,
+  request,
   user,
+  isRequestToAcceptOrDeny = false,
   handleSubmit,
+  handleDenySubmit,
 }: {
   book: IBook;
+  request?: IRequest;
   user?: IMeUser | null;
-  handleSubmit: () => void;
+  isRequestToAcceptOrDeny?: boolean;
+  handleSubmit?: () => void;
+  handleDenySubmit?: () => void;
 }) {
+  const isRegularUser = user?.role === UserRoleEnum.USER;
+
   return (
-    <div className="w-[300px] min-h-[320px] bg-white rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:shadow-2xl dark:bg-gray-950 cursor-pointer">
+    <div className="w-full min-h-[290px] bg-white rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:shadow-2xl dark:bg-gray-950 cursor-pointer">
       <img
         src={book.url}
         alt="Product Image"
@@ -25,15 +35,37 @@ export default function BookCard({
       <div className="flex-1 p-4 space-y-2 flex flex-col justify-between">
         <h3 className="text-xl font-semibold">{book.name}</h3>
 
-        <div className="mt-auto flex justify-between items-center text-sm text-gray-500 dark:text-gray-400">
-          <span>{book.language.name}</span>
-          <span className="font-bold">{book.publicationYear}</span>
-        </div>
+        {book?.language?.name || book?.publicationYear ? (
+          <div className="mt-auto flex justify-between items-center text-sm text-gray-500 dark:text-gray-400">
+            {book?.language?.name && <span>{book.language.name}</span>}
+            {book?.publicationYear && (
+              <span className="font-bold">{book.publicationYear}</span>
+            )}
+          </div>
+        ) : null}
 
-        {user?.role === UserRoleEnum.USER && (
-          <Button onClick={handleSubmit} className="w-full">
-            Request Book
-          </Button>
+        {((isRegularUser && !request) || !user) && (
+          <Button onClick={handleSubmit}>Request Book</Button>
+        )}
+
+        {(request?.status === StatusRequestEnum.APPROVAL ||
+          request?.status === StatusRequestEnum.BORROWED) &&
+          isRegularUser &&
+          request && (
+            <div className="mt-auto flex justify-end items-center text-sm text-gray-500 dark:text-gray-400">
+              <Button onClick={handleSubmit}>
+                {request?.status === StatusRequestEnum.APPROVAL
+                  ? "Borrow"
+                  : "Return"}
+              </Button>
+            </div>
+          )}
+
+        {user?.role === UserRoleEnum.EMPLOYEE && isRequestToAcceptOrDeny && (
+          <div className="mt-auto flex justify-between items-center text-sm text-gray-500 dark:text-gray-400">
+            <Button onClick={handleSubmit}>Approve</Button>
+            <Button onClick={handleDenySubmit}>Deny</Button>
+          </div>
         )}
       </div>
     </div>
