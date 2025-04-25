105655f46afc23eed303c62e4f0580fb6eb7508d
TommyGC11 <tommycontreras34@gmail.com>
2025-04-25 13:01:54 -0400

    fix: redirect to home instead of sign-in for unauthorized access to protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -39,7 +39,7 @@ export async function middleware(request: NextRequest) {
   }
 
   if (isProtectedRoute(currentPath) && !user) {
-    return NextResponse.redirect(new URL("auth/signIn", request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   let allowedRoutes = [...protectedRoutes];
